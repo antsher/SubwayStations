e2f3869e5c9c786dbd87d6b7276ec8c7478123db
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-10-19 16:20:55 +0300

    corrected DaggerApplication import package from dagger.android to dagger.android.support

--- app/src/main/java/com/stazis/subwaystations/SubwayStationsApplication.kt
+++ app/src/main/java/com/stazis/subwaystations/SubwayStationsApplication.kt
@@ -2,7 +2,7 @@ package com.stazis.subwaystations
 
 import com.stazis.subwaystations.di.components.DaggerApplicationComponent
 import dagger.android.AndroidInjector
-import dagger.android.DaggerApplication
+import dagger.android.support.DaggerApplication
 
 class SubwayStationsApplication : DaggerApplication() {
 
