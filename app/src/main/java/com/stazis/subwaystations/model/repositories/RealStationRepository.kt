65c377edcf515ce49912dcfe70f751471c2207f1
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-10-24 19:52:32 +0300

    added multidex support; added checking coordinates in RealStationRepository, refactored code to be a more 'Kotlin way'

--- app/src/main/java/com/stazis/subwaystations/model/repositories/RealStationRepository.kt
+++ app/src/main/java/com/stazis/subwaystations/model/repositories/RealStationRepository.kt
@@ -24,20 +24,24 @@ class RealStationRepository(
 
     private fun loadStationsFromNetwork(emitter: SingleEmitter<List<Station>>) = try {
         val stations = stationService.getStations().execute().body()
-        if (stations != null) {
-            stationDao.insertAll(stations)
-            emitter.onSuccess(stations)
-        } else {
-            emitter.onError(Exception("No data received!"))
-        }
+        stations?.toMutableList()?.apply {
+            map { station -> ifIncorrectCoordinates(station) { remove(station) } }
+            stationDao.insertAll(this)
+            emitter.onSuccess(this)
+        } ?: emitter.onError(Exception("No data received!"))
     } catch (exception: Exception) {
         emitter.onError(exception)
     }
 
-    private fun loadStationsFromDatabase(emitter: SingleEmitter<List<Station>>) {
-        val stations = stationDao.getAll()
-        if (!stations.isEmpty()) {
-            emitter.onSuccess(stations)
+    private fun ifIncorrectCoordinates(station: Station, f: () -> Unit) {
+        if (station.latitude == 0.0 || station.longitude == 0.0) {
+            f()
+        }
+    }
+
+    private fun loadStationsFromDatabase(emitter: SingleEmitter<List<Station>>) = stationDao.getAll().apply {
+        if (!isEmpty()) {
+            emitter.onSuccess(this)
         } else {
             emitter.onError(Exception("Database is empty!"))
         }
@@ -53,22 +57,23 @@ class RealStationRepository(
         }
 
     private fun loadStationFromNetwork(emitter: SingleEmitter<Station>, stationName: String) = try {
-        val stations = stationService.getStations().execute().body()
-        if (stations != null) {
-            emitter.onSuccess(stations.find { it -> it.name == stationName }!!)
-        } else {
-            emitter.onError(Exception("No data received!"))
+        stationService.getStations().execute().body().apply {
+            if (this != null) {
+                emitter.onSuccess(find { station -> station.name == stationName }!!)
+            } else {
+                emitter.onError(Exception("No data received!"))
+            }
         }
     } catch (exception: Exception) {
         emitter.onError(exception)
     }
 
-    private fun loadStationFromDatabase(emitter: SingleEmitter<Station>, stationName: String) {
-        val station = stationDao.get(stationName)
-        if (station != null) {
-            emitter.onSuccess(station)
-        } else {
-            emitter.onError(Exception("Station with name $stationName not found!"))
+    private fun loadStationFromDatabase(emitter: SingleEmitter<Station>, stationName: String) =
+        stationDao.get(stationName).apply {
+            if (this != null) {
+                emitter.onSuccess(this)
+            } else {
+                emitter.onError(Exception("Station with name $stationName not found!"))
+            }
         }
-    }
 }
