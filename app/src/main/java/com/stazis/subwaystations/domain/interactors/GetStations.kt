16359be8726acefb7ad1d194ad0a1afb66eb5a55
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-10-24 15:39:40 +0300

    minor refactoring

--- app/src/main/java/com/stazis/subwaystations/domain/interactors/GetStations.kt
+++ app/src/main/java/com/stazis/subwaystations/domain/interactors/GetStations.kt
@@ -8,7 +8,8 @@ import javax.inject.Inject
 class GetStations @Inject constructor(private val stationRepository: StationRepository) {
 
     fun execute(): Single<List<Station>> = stationRepository.getStations().map { stationList: List<Station>? ->
-        val items = stationList ?: emptyList()
-        items.map { Station(it.name, it.latitude, it.longitude) }
+        (stationList ?: emptyList()).apply {
+            map { Station(it.name, it.latitude, it.longitude) }
+        }
     }
 }
