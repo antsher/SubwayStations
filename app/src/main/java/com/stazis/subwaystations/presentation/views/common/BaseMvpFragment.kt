ab2ab57b0973e57fb55ffe79f37ddc65d5db972a
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-11-08 10:21:53 +0300

    removed unused instanceState functions from BaseMvpFragment

--- app/src/main/java/com/stazis/subwaystations/presentation/views/common/BaseMvpFragment.kt
+++ app/src/main/java/com/stazis/subwaystations/presentation/views/common/BaseMvpFragment.kt
@@ -8,30 +8,14 @@ import android.view.View.VISIBLE
 import android.view.ViewGroup
 import com.stazis.subwaystations.R
 import com.stazis.subwaystations.presentation.presenters.BasePresenter
-import com.stazis.subwaystations.presentation.views.common.instancestate.NotNullStateProvider
-import com.stazis.subwaystations.presentation.views.common.instancestate.NullableStateProvider
 import com.stazis.subwaystations.presentation.views.common.moxyandroidx.MoxyAppCompatFragment
 
 abstract class BaseMvpFragment<Presenter : BasePresenter<out BaseView>> : MoxyAppCompatFragment(), BaseView {
 
-    companion object {
-
-        private const val STATE_BUNDLE_KEY = "STATE_BUNDLE_KEY"
-    }
-
     abstract var presenter: Presenter
-    private val stateBundle = Bundle()
     private lateinit var progressBar: View
     private lateinit var messageDialog: AlertDialog
     open lateinit var root: ViewGroup
-
-    protected fun <T> instanceState() = NullableStateProvider<T>(stateBundle)
-    protected fun <T> instanceState(defaultValue: T) = NotNullStateProvider(stateBundle, defaultValue)
-
-    override fun onCreate(savedInstanceState: Bundle?) = with(savedInstanceState) {
-        this?.let { stateBundle.putAll(getBundle(STATE_BUNDLE_KEY)) }
-        super.onCreate(this)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         progressBar = layoutInflater.inflate(R.layout.view_progress_bar, root, false).apply { root.addView(this) }
@@ -57,10 +41,5 @@ abstract class BaseMvpFragment<Presenter : BasePresenter<out BaseView>> : MoxyAp
 
     override fun hideLoading() {
         progressBar.visibility = GONE
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) = with(outState) {
-        putBundle(STATE_BUNDLE_KEY, stateBundle)
-        super.onSaveInstanceState(this)
     }
 }
