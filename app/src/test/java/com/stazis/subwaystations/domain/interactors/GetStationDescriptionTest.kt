a86cabfdf2e8ac8cd958daa6bd240334b4579832
Anton Sheremet <anton.sheremet@itechart-group.com>
2019-01-09 18:43:16 +0300

    added GetStationDescriptionTest class

---
+++ app/src/test/java/com/stazis/subwaystations/domain/interactors/GetStationDescriptionTest.kt
@@ -0,0 +1,48 @@
+package com.stazis.subwaystations.domain.interactors
+
+import com.stazis.subwaystations.model.repositories.StationRepository
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
+private const val STATION_NAME = "STATION_NAME"
+
+class GetStationDescriptionTest {
+
+    @Mock
+    lateinit var mockStationRepository: StationRepository
+
+    private val getStationDescription by lazy { GetStationDescription(mockStationRepository) }
+
+    @Before
+    fun setUp() = MockitoAnnotations.initMocks(this)
+
+    @Test
+    fun `getStationDescription IfEmptyString ReturnsEmptyString`() {
+        "".let { emptyString ->
+            `when`(mockStationRepository.getStationDescription(STATION_NAME))
+                .thenReturn(Single.create<String> { it.onSuccess(emptyString) })
+
+            with(getStationDescription.execute(STATION_NAME).test()) {
+                assertNoErrors()
+                assertValue { it == emptyString }
+            }
+        }
+    }
+
+    @Test
+    fun `getStationDescription IfNonEmptyString ReturnsSameString`() {
+        "Description".let { emptyString ->
+            `when`(mockStationRepository.getStationDescription(STATION_NAME))
+                .thenReturn(Single.create<String> { it.onSuccess(emptyString) })
+
+            with(getStationDescription.execute(STATION_NAME).test()) {
+                assertNoErrors()
+                assertValue { it == emptyString }
+            }
+        }
+    }
+}
